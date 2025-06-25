fd9a4809cbfbe65b1a99d01020b219d781cca02e
gaurav jichkar <127932525+Gaurav12jichkar@users.noreply.github.com>
2025-06-25 12:22:13 +0530

    gaurav

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
-const connectDB=async() =>{
-try{
-const connectionInstance = await mongoose.connect('${process.env.MONGO_URI}',console.log("mongodb connected"))
 
-}
-catch(error){
-    console.log("Mongodb failed",console.error);
-    process.exit(1)
-}
-}
-export default connectDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB connected");
+  } catch (error) {
+    console.log("❌ MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
